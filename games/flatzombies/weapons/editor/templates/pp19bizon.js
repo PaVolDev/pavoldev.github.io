05cee00888217e201906e1f9ca5d094f357a131f
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2025-11-27 14:34:25 +0500

    Upload pp19bizon.js

--- games/flatzombies/weapons/editor/templates/pp19bizon.js
+++ games/flatzombies/weapons/editor/templates/pp19bizon.js
@@ -177,5 +177,6 @@ export default {
 "weapon.magazine.SpriteRenderer.enabled": true,
 "weapon.magazine.name": "magazine",
 "weapon.name": "pp19bizon",
-"storeInfo.silencerGroup": "pistol"
+"storeInfo.silencerGroup": "pistol",
+"shotAnimations[0].animation": "fire",
 };
