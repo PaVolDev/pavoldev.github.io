9f08a687e6c1d633e9c839ea84c5e15bbb6145a8
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2025-11-27 14:26:04 +0500

    Upload deagle.js

--- games/flatzombies/weapons/editor/templates/deagle.js
+++ games/flatzombies/weapons/editor/templates/deagle.js
@@ -195,5 +195,6 @@ export default {
 "weapon.magazine.SpriteRenderer.enabled": true,
 "weapon.magazine.name": "magazine",
 "weapon.name": "deagle",
-"storeInfo.silencerGroup": "pistol"
+"storeInfo.silencerGroup": "pistol",
+"shotAnimations[0].animation": "LabelShotFire",
 };
