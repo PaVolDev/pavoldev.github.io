27253fbd9913712105a16f06fc72e9a086c9a589
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2025-11-27 14:27:17 +0500

    Upload m110.js

--- games/flatzombies/weapons/editor/templates/m110.js
+++ games/flatzombies/weapons/editor/templates/m110.js
@@ -204,5 +204,6 @@ export default {
 "weapon.handgrip.SpriteRenderer.enabled": true,
 "weapon.handgrip.name": "handgrip",
 "weapon.name": "m110",
-"storeInfo.silencerGroup": "rifle"
+"storeInfo.silencerGroup": "rifle",
+"shotAnimations[0].animation": "fire",
 };
