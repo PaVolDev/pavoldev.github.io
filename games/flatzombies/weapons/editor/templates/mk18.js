ba042aa15a1ea258b417c4b495765f64b901679e
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2025-11-27 14:27:25 +0500

    Upload mk18.js

--- games/flatzombies/weapons/editor/templates/mk18.js
+++ games/flatzombies/weapons/editor/templates/mk18.js
@@ -186,5 +186,6 @@ export default {
 "weapon.handgrip.SpriteRenderer.enabled": false,
 "weapon.handgrip.name": "handgrip",
 "weapon.name": "mk18",
-"storeInfo.silencerGroup": "rifle"
+"storeInfo.silencerGroup": "rifle",
+"shotAnimations[0].animation": "fire",
 };
