36a2ddd6a17fb2c2315bdcd80315d005edba914e
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2025-11-27 14:34:17 +0500

    Upload scarlcqc.js

--- games/flatzombies/weapons/editor/templates/scarlcqc.js
+++ games/flatzombies/weapons/editor/templates/scarlcqc.js
@@ -186,5 +186,6 @@ export default {
 "weapon.handgrip.SpriteRenderer.enabled": true,
 "weapon.handgrip.name": "handgrip",
 "weapon.name": "scarlcqc",
-"storeInfo.silencerGroup": "rifle"
+"storeInfo.silencerGroup": "rifle",
+"shotAnimations[0].animation": "fire",
 };
