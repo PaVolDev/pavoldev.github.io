b878f4f8779eb472106ed0ed194eaf5e95c8cb71
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-02-21 05:44:07 +0500

    Upload pt.js

---
+++ games/flatzombies/weapons/editor/list/lang/pt.js
@@ -0,0 +1,14 @@
+window.translate = Object.assign(window.translate, {
+	1004: "Lista de mods",
+	1005: "Erro de conexão com o servidor",
+	1006: "Autenticando...",
+	1007: "Usuário",
+	1008: "Senha",
+	1009: "Salvar no servidor compartilhado",
+	1010: "Entrar",
+	1011: "Sair",
+	1012: "Editar",
+	1013: "Arquivo",
+	1014: "Excluir",
+	1015: "Ações..."
+});
