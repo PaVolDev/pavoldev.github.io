0c1c57b2bc54c4c697202a5e69c1ee731fb0dd16
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-05-22 02:51:58 +0500

    Upload parameters.js

--- games/flatzombies/weapons/editor/parameters.js
+++ games/flatzombies/weapons/editor/parameters.js
@@ -1000,8 +1000,8 @@ function getInputHTML(param, index = -1, objKey = null, objMetaData = null) {
 		case 'int':
 			if ('min' in param && 'max' in param) {
 				return `<div style="display: grid; grid-template-columns: 65% 30%; align-items: center; justify-content: space-between;">
-					<input type="range" min="${param.min}" max="${param.max}" step="0.01" id="${param.startFieldPath}Slider" oninput="updateFieldHTML(${index}, this.value)" value="${param.value}">
-					<input type="number" min="${param.min}" max="${param.max}"  placeholder="${param.placeholder || param.type}" id="${param.startFieldPath}" oninput="updateParam('${param.startFieldPath}', this.value, false, '${objKey || ''}')" value="${param.value}" >
+					<input type="range" step="${param.type == 'int' ? "1" : "0.01"}" min="${param.min}" max="${param.max}" id="${param.startFieldPath}Slider" oninput="updateFieldHTML(${index}, this.value)" value="${param.value}">
+					<input type="number" step="1" min="${param.min}" max="${param.max}"  placeholder="${param.placeholder || param.type}" id="${param.startFieldPath}" oninput="updateParam('${param.startFieldPath}', this.value, false, '${objKey || ''}')" value="${param.value}" >
 				</div>`;
 			}
 			return `<input type="number" value="${param.value}" ${param.min ? `min="${param.min}"` : ''} ${param.max ? `max="${param.max}"` : ''} onchange="updateParam('${param.startFieldPath}', this.value, false, '${objKey || ''}')" id="${param.startFieldPath}" class="field-input" data-tooltip="${param.type}" >`;
