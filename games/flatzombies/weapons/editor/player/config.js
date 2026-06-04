eb04f96f543dde74a415c27376cf19941d9f2412
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-04 15:57:51 +0500

    Upload games/flatzombies/weapons/editor/player/config.js

--- games/flatzombies/weapons/editor/player/config.js
+++ games/flatzombies/weapons/editor/player/config.js
@@ -258,7 +258,7 @@ var baseParams = [  //Список параметров, доступные дл
 	{ fieldPath: "storeInfo.iconBase64.pivotPoint", comment: "Точка вращения для спрайта", type: "Vector2", value: "(0.5, 0.5)" },
 	{ fieldPath: "storeInfo.iconBase64.pixelPerUnit", comment: "Плотность пикселей", type: "float", value: 100 },
 	{ fieldPath: "interface.iconImage.rectTransform.localScale", comment: "Масштаб иконки на кнопке в интерфейсе\nМенять только при использовании своего iconBase64", type: "Vector3", value: "(0.42, 0.42, 0.42)" },
-	{ fieldPath: "interface.Image.sprite", comment: "Фоновое изображение для кнопки в интерфейсе, 180x264px<br>Кнопка имеет переключение цвета и этот цвет накладывается на текстуру в режиме 'Умножение' - белые пиксели текустуры будут полностью окрашиваться в цвет кнопки.", type: "TextureSprite", value: "" },
+	{ fieldPath: "interface.Image.sprite", comment: "Фоновое изображение для кнопки в интерфейсе, 180x264px, <span class=\"show-tooltip\" data-tooltip=\"Кнопка имеет переключение цвета и этот цвет накладывается на текстуру в режиме 'Умножение' - белые пиксели текустуры будут полностью окрашиваться в цвет кнопки.\">без альфа-канала.</span>", type: "TextureSprite", value: "" },
 	{ fieldPath: "player.addedGameObjects", comment: "Список добавленных объектов", type: "string", value: "" },
 	{ fieldPath: "player.addedComponents", comment: "Список добавленных компонентов MonoBehaviour", type: "string", value: "" }, //в формате "child.SpriteRenderer, otherChild.Collider2D"
 	{ fieldPath: "player.removedGameObjects", comment: "Список объектов для удаления", type: "string", value: "" },
