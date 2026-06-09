6476ddcc017f1aca33ae52d1bc13ac4391607ae9
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-09 20:02:57 +0500

    Upload games/flatzombies/weapons/editor/templates/at308.js

--- games/flatzombies/weapons/editor/templates/at308.js
+++ games/flatzombies/weapons/editor/templates/at308.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.23, 0)",
 "weapon.RifleWithMagazine.flashlight":"(1.08, -0.03, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"338LM",
