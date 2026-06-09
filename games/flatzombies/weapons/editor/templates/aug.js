44d8ef72509ab50426b5f03658bdb1c300bb6aec
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-09 20:02:59 +0500

    Upload games/flatzombies/weapons/editor/templates/aug.js

--- games/flatzombies/weapons/editor/templates/aug.js
+++ games/flatzombies/weapons/editor/templates/aug.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.2, 0)",
 "weapon.RifleWithMagazine.flashlight":"(0.24, 0.22, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"55645",
