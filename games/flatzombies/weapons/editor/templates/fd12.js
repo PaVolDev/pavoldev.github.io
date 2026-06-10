81a828c3225cb4731f8fc677a59f4ab14b6195f9
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-11 01:08:14 +0500

    Upload games/flatzombies/weapons/editor/templates/fd12.js

--- games/flatzombies/weapons/editor/templates/fd12.js
+++ games/flatzombies/weapons/editor/templates/fd12.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.16, 0)",
 "weapon.RifleWithMagazine.flashlight":"(0.14, 0.41, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"1270",
