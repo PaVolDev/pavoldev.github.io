7cf8f0b45426a482f863daa96288f29a6d655f22
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-11 01:09:18 +0500

    Upload games/flatzombies/weapons/editor/templates/imbelai2.js

--- games/flatzombies/weapons/editor/templates/imbelai2.js
+++ games/flatzombies/weapons/editor/templates/imbelai2.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.18, 0)",
 "weapon.RifleWithMagazine.flashlight":"(1.07, 0.18, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"55645",
