36b8f4efb64cf30e9ab4b1e767aad184790c4b41
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-11 01:09:20 +0500

    Upload games/flatzombies/weapons/editor/templates/lr300.js

--- games/flatzombies/weapons/editor/templates/lr300.js
+++ games/flatzombies/weapons/editor/templates/lr300.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.3, 0)",
 "weapon.RifleWithMagazine.flashlight":"(1.13, 0.11, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"55645",
