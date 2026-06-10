1084c48069708ca4ccf9db43bddf2d2ae087bebe
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-11 01:09:25 +0500

    Upload games/flatzombies/weapons/editor/templates/m200.js

--- games/flatzombies/weapons/editor/templates/m200.js
+++ games/flatzombies/weapons/editor/templates/m200.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.34, 0)",
 "weapon.RifleWithMagazine.flashlight":"(0.68, 0.22, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"408CT",
