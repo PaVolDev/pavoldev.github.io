04b3d9a47e515a3e6dad23f00ecce7fdb5508574
PaVolDev <40880620+PaVolDev@users.noreply.github.com>
2026-06-11 01:09:05 +0500

    Upload games/flatzombies/weapons/editor/templates/rem870dm.js

--- games/flatzombies/weapons/editor/templates/rem870dm.js
+++ games/flatzombies/weapons/editor/templates/rem870dm.js
@@ -34,7 +34,7 @@ export default {
 "weapon.RifleWithMagazine.shellDrop.quantity": 1,
 "weapon.RifleWithMagazine.bolt":"(0, 0.15, 0)",
 "weapon.RifleWithMagazine.flashlight":"(1.1, 0.1, 0)",
-"weapon.RifleWithMagazine.flashlightParent":"",
+"weapon.RifleWithMagazine.flashlightParent":"", "weapon.RifleWithMagazine.laserEnabled":true, "weapon.LineRenderer.color":"#FF0000",
 "weapon.RifleWithMagazine.flashlightSkin.name": "!СветУзкий",
 "weapon.RifleWithMagazine.flashlightSkin.hideFlags.value__": 0,
 "weapon.RifleWithMagazine.cartridge":"1270",
